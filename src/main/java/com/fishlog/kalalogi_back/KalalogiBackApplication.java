a798f35bedc5d73263495f3b5b6a6c340824b6f5
Kuldar Nei <kuldarnei@gmail.com>
2023-02-06 14:13:57 +0200

    Initial Commit

---
+++ src/main/java/com/fishlog/kalalogi_back/KalalogiBackApplication.java
@@ -0,0 +1,13 @@
+package com.fishlog.kalalogi_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalalogiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalalogiBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fishlog/kalalogi_back/KalalogiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishlog.kalalogi_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalalogiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
