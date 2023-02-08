fe820454d2e79262aeab261c9f5426f5720c3857
pbernotas <priit.bernotas@gmail.com>
2023-02-08 14:56:27 +0200

    Added DB

---
+++ src/main/java/com/fishlog/kalalogi_back/FishController.java
@@ -0,0 +1,11 @@
+package com.fishlog.kalalogi_back;
+
+
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@Resource
+public class FishController {
+}
