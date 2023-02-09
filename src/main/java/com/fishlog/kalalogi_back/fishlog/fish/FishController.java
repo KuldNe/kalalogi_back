faad3acfdd2d7a0aa49e5a5445e256487627e7d1
Marii <mtelliskivi@gmail.com>
2023-02-09 09:03:21 +0200

    commit

--- src/main/java/com/fishlog/kalalogi_back/fishlog/fish/FishController.java
+++ src/main/java/com/fishlog/kalalogi_back/fishlog/fish/FishController.java
@@ -1,11 +1,22 @@
 package com.fishlog.kalalogi_back.fishlog.fish;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 
 @Resource
 public class FishController {
 }
+
+    @GetMapping("/catchlocations")
+    @Operation(summary = "Finds all catch locations from system/database", description = "This information is used in frontend to create catch locations dropdown")
+    public List<CityDto> getAllCities() {
+        List<CityDto> cities = atmService.getAllCities();
+        return cities;
+    }
