52a67241ccd66f5f897564e41230b76fcd0024a5
Kuldar Nei <kuldarnei@gmail.com>
2023-02-10 09:21:57 +0200

    Marii ja Kuldar Merge

--- src/main/java/com/fishlog/kalalogi_back/domain/waterbody/WaterbodyService.java
+++ src/main/java/com/fishlog/kalalogi_back/domain/waterbody/WaterbodyService.java
@@ -1,4 +1,19 @@
 package com.fishlog.kalalogi_back.domain.waterbody;
 
+import com.fishlog.kalalogi_back.domain.species.Species;
+import com.fishlog.kalalogi_back.fishlog.Status;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class WaterbodyService {
+    @Resource
+    private WaterbodyRepository waterbodyRepository;
+
+    public List<Waterbody> findAllWaterbodies() {
+        return waterbodyRepository.findWaterbodies(Status.ACTIVE);
+    }
+
 }
--- src/main/java/com/fishlog/kalalogi_back/fishlog/location/LocationController.java
+++ src/main/java/com/fishlog/kalalogi_back/fishlog/location/LocationController.java
@@ -1,14 +1,13 @@
 package com.fishlog.kalalogi_back.fishlog.location;
 
-import com.fishlog.kalalogi_back.domain.user.UserService;
-import com.fishlog.kalalogi_back.domain.waterbody.WaterbodyRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.stream.Location;
 import java.util.List;
 
+@RestController
 public class LocationController {
 
     @Resource
@@ -16,12 +15,9 @@ public class LocationController {
 
     @GetMapping("/waterbodies")
     @Operation(summary = "Find all waterbodies from the database", description = "This is used for the waterbodies dropdown")
-    public List<LocationDto> getAllLocations() { return LocationService.getAllLocations();
-
+    public List<LocationDto> getAllLocations() {
+        return locationService.getAllLocations();
     }
-
-//      public List<SpeciesDto>  getAllSpecies() { return fishLogService.getAllSpecies();
-
 
 
 }
