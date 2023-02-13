41b89f4cde25abba23d4ae58d0fb73f54477f3fa
pbernotas <priit.bernotas@gmail.com>
2023-02-13 16:47:40 +0200

    DB update

--- src/main/java/com/fishlog/kalalogi_back/domain/user/UserMapper.java
+++ src/main/java/com/fishlog/kalalogi_back/domain/user/UserMapper.java
@@ -1,7 +1,9 @@
 package com.fishlog.kalalogi_back.domain.user;
 
 import com.fishlog.kalalogi_back.domain.user.User;
+import com.fishlog.kalalogi_back.fishlog.Status;
 import com.fishlog.kalalogi_back.fishlog.login.LoginResponseDTO;
+import com.fishlog.kalalogi_back.fishlog.register.RegisterDto;
 import org.mapstruct.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
@@ -13,6 +15,17 @@ public interface UserMapper {
     @Mapping(source = "role.name", target = "roleType")
     LoginResponseDTO toDto(User user);
 
+
+    @Mapping(source = "userName", target = "username")
+    @Mapping(source = "email", target = "eMail")
+    @Mapping(source = "password", target = "password")
+    @Mapping(constant = Status.ACTIVE, target = "status")
+    User registerEntity(RegisterDto registerDto);
+
+
+
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     User partialUpdate(LoginResponseDTO loginResponseDTO, @MappingTarget User user);
+
+
 }
---
+++ src/main/java/com/fishlog/kalalogi_back/fishlog/register/RegisterDto.java
@@ -0,0 +1,15 @@
+package com.fishlog.kalalogi_back.fishlog.register;
+
+
+import lombok.Data;
+
+@Data
+public class RegisterDto {
+
+    private String userName;
+
+    private String email;
+
+    private String password;
+
+}
